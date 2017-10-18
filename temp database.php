000e51001b485ea5e05a1267a8fa7f55b440d85c
R. Vonk <robin.vonk@hva.nl>
2017-10-19 00:51:44 +0200

    temp index voor de database

--- temp database.php
+++ temp database.php
@@ -1,36 +1,3 @@
-<?php
-    if (Isset($_FILES['uploadfoto'])){
-        //variabelen voor de upload
-        $foto = $_FILES['uploadfoto'];
-        $uploadlocatie = "foto/". basename($_FILES["fileToUpload"]["name"]);
-        print_r($foto);
-
-        // Verbinding creeeren met de database
-        $link = new mysqli(localhost, prinses, prinses1337, prinsesdb);
-        // Error check voor de verbinding
-        if ($link->connect_error) {
-            die("Connection failed: " . $link->connect_error);
-        }
-        
-        //collect de data uit de form
-        
-        
-        //insert data naar de database
-        $insertdata = "INSERT INTO foto (bestand)
-                VALUES(1,LOAD_FILE('$foto')";
-        
-        mysqli_query($link, $insertdata);
-        
-        if(move_uploaded_file($_FILES['uploadfoto']['tmp_name'], $uploadlocatie)){
-            echo 'success';
-        }else{
-            echo 'failed';
-        }
-
-        //verbinding met de database verbreken  
-        $link->close();
-    }
-?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -41,11 +8,20 @@
         <!--form voor de upload van de foto -->
         <form method="post" action="index.php" enctype="multipart/form-data">
             Select image to upload:
-            <input type="file" name="uploadfoto">
-            <input type="submit" name="submit" value="Ding uploaden">
+            <input type="file" name="foto">
+            <input type="submit" name="upload">
         </form>
-
-        
-        
+        <form method="post" action="index.php">
+            <input type="number" name="code" value="Geef hier uw code">
+            <input type="submit" name="invoer">
+        </form>
+        <?php
+        if (Isset($_POST['upload'])){
+        include 'fileupload.php';
+        }
+        if (Isset($_POST['invoer'])){
+        include 'filedownload.php';
+        }
+        ?>
     </body>
 </html>
