9af511a5f14441627a61ca411116fffadd2361ae
R.M. Blom <rutger.blom2@hva.nl>
2017-10-18 19:38:21 +0200

    2e pagina geupload

---
+++ tweedepagina.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            table, th, td {
+                border-collapse: collapse;
+            }
+            th, td {
+                padding: 5px;
+            }
+            table#t01 {
+                width: 100%;    
+                background-color: #f1f1c1;
+            }
+        </style>
+        <meta charset="UTF-8">
+
+    </head>
+    <body>
+        <img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/5f3dc6dce1b1f025cad85efaf9610a5463345c3a/foto/logocorendon.jpg" alt="Corendon logo" width="600" height="200"> 
+
+
+        <table style="width:100%">
+            <tr>
+                <td align="right"><img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/master/foto/Test.jpg" alt="Foto 1" width="450" height="400"> </td>
+                <td><img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/master/foto/Test.jpg" alt="Foto 2" width="450" height="400"> </td>
+            </tr>
+            <tr>
+                <td align="right"><img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/master/foto/Test.jpg" alt="Foto 3" width="450" height="400"></td>
+                <td><img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/master/foto/Test.jpg" alt="Foto 4" width="450" height="400"></td>
+            </tr>
+            <tr>
+                <td align="right"><img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/master/foto/Test.jpg" alt="Foto 5" width="450" height="400"></td>
+                <td><img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/master/foto/Test.jpg" alt="Foto 6" width="450" height="400"></td>
+            </tr>
+        </table>
+
+    </body>
+</html>
