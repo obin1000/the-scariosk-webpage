da9093373c19f4cfde77a6cd5db893216d7121ce
R. Vonk <robin.vonk@hva.nl>
2017-10-18 18:52:07 +0200

    backup van de grogress

--- temp database.php
+++ temp database.php
@@ -1,3 +1,36 @@
+<?php
+    if (Isset($_FILES['uploadfoto'])){
+        //variabelen voor de upload
+        $foto = $_FILES['uploadfoto'];
+        $uploadlocatie = "foto/". basename($_FILES["fileToUpload"]["name"]);
+        print_r($foto);
+
+        // Verbinding creeeren met de database
+        $link = new mysqli(localhost, prinses, prinses1337, prinsesdb);
+        // Error check voor de verbinding
+        if ($link->connect_error) {
+            die("Connection failed: " . $link->connect_error);
+        }
+        
+        //collect de data uit de form
+        
+        
+        //insert data naar de database
+        $insertdata = "INSERT INTO foto (bestand)
+                VALUES(1,LOAD_FILE('$foto')";
+        
+        mysqli_query($link, $insertdata);
+        
+        if(move_uploaded_file($_FILES['uploadfoto']['tmp_name'], $uploadlocatie)){
+            echo 'success';
+        }else{
+            echo 'failed';
+        }
+
+        //verbinding met de database verbreken  
+        $link->close();
+    }
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,29 +38,14 @@
         <title>Die ene kiosk</title>
     </head>
     <body>
-                <?php
-                $servername = "localhost";
-                $username = "prinses";
-                $password = "prinses1337";
-                $dbname ="prinsesdb";
+        <!--form voor de upload van de foto -->
+        <form method="post" action="index.php" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="uploadfoto">
+            <input type="submit" name="submit" value="Ding uploaden">
+        </form>
 
-                // Create connection
-                $conn = new mysqli($servername, $username, $password, $dbname);
-                // Check connection
-                if ($conn->connect_error) {
-                    die("Connection failed: " . $conn->connect_error);
-                 }
-                if(isset($_POST[naam])){
-                    $sql = "INSERT INTO prinsje (naam,leeftijd)
-                    VALUES ('$_POST[naam]', '$_POST[leeftijd]')";
-                    
-                if ($conn->query($sql) === TRUE) {
-                    echo "New record created successfully";
-                } else {
-                    echo "Error: " . $sql . "<br>" . $conn->error;
-                }
-                 }
-                 $conn->close();
-                ?>
+        
+        
     </body>
 </html>
