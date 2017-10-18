6d0cc8182d2756aebdbde26d15154a799dc902b8
R. Vonk <robin.vonk@hva.nl>
2017-10-19 00:51:07 +0200

    PHP om bestanden naar de database te uploaden

---
+++ fileupload.php
@@ -0,0 +1,33 @@
+<?php
+//variables als opslag van de data van de file
+$foto = $_FILES['foto'];            
+$foto_naam = $foto['name'];         //orginele bestandsnaam
+$foto_tmp = $foto['tmp_name'];      //tijdelijke opslag
+$foto_error = $foto['error'];       //error counter
+$foto_id = rand(0, 999999999);      //uniek id voor de database
+shuffle($foto_id);
+$foto_locatie = 'foto/' . $foto_id . '.jpg';    //path naar /foto als id.jpg
+
+if($foto_error > 0){                //check voor errors
+    die("Error uploading" . $error);
+}
+else{
+    //als er geen eroors zijn wordt de afbeelding geupload
+    if (move_uploaded_file($foto_tmp, $foto_locatie)){
+        //De klant de id laten zin waarmee de foto kan worden opgehaald
+        echo "<h1>$foto_id</h1>";   
+    }
+}
+//verbinding maken met de lokale database
+$conn = new mysqli("localhost", "prinses", "prinses1337", "prinsesdb") or die(mysql_error());
+
+//query voor heet invullen van de ID en locatie van de foto
+$sql = "INSERT INTO foto (id,path) VALUES ('$foto_id','$foto_locatie')";
+
+if ($conn->query($sql) === TRUE) {
+} 
+else {  //errorcheck voor het toevoegen aan de database
+    echo "failed adding to database: " . $sql . "<br>" . $conn->error;
+}
+//verbreken van de verbinding met de database
+$conn->close();
