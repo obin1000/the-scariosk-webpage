72ce1f84d93f6925bbc5c5b818ad6096850149d1
M. El Hadiyen <mohamed.el.hadiyen@hva.nl>
2017-10-20 18:50:54 +0200

    Update tweedepagina.php: code verbeterd.

--- tweedepagina.php
+++ tweedepagina.php
@@ -7,8 +7,8 @@
     <body>
         <img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/5f3dc6dce1b1f025cad85efaf9610a5463345c3a/foto/logocorendon.jpg" alt="Corendon logo" width="600" height="200"> 
 
-
-        <table style="width:100%">
+		<div style="overflow-x:auto;">
+        <table>
             <tr>
                 <td align="right"><img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/master/foto/Test.jpg" alt="Foto 1" width="450" height="400"> </td>
                 <td><img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/master/foto/Test.jpg" alt="Foto 2" width="450" height="400"> </td>
@@ -22,6 +22,7 @@
                 <td><img src="https://gitlab.fdmci.hva.nl/fys34/webpage/raw/master/foto/Test.jpg" alt="Foto 6" width="450" height="400"></td>
             </tr>
         </table>
+		</div>
 
     </body>
 </html>
