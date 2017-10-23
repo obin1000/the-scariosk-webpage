a6a917a164d3a3413866088444265f5ea655bae2
R. Vonk <robin.vonk@hva.nl>
2017-10-23 17:02:04 +0200

    Toevoeging van comments

--- filedownload.php
+++ filedownload.php
@@ -1,8 +1,14 @@
 <?php
-$code = $_POST['code']; 
+//inlezen van de ingevoerde code
+$code = $_POST['code'];
+//verbinding met de database maken
 $link = new mysqli("localhost", "prinses", "prinses1337", "prinsesdb") or die(mysql_error());
+//query voor het selecteren van de foto
 $query = "SELECT path FROM foto WHERE id=$code";
+//uitvoeren van de query
 $exquery = $link->query($query);
+//verzamelen van de resultaten
 $row = $exquery-> fetch_assoc();
+//variable voor het oproepen van de foto
 $foto_path= $row['path'];
 $link->close();
