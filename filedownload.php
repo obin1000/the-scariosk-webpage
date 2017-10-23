1540dbb62bed223aaf013b1e045a42e3cbc56b48
R. Vonk <robin.vonk@hva.nl>
2017-10-23 16:59:35 +0200

    Werkend gemaakt

--- filedownload.php
+++ filedownload.php
@@ -1,17 +1,8 @@
 <?php
 $code = $_POST['code']; 
 $link = new mysqli("localhost", "prinses", "prinses1337", "prinsesdb") or die(mysql_error());
-$query = "SELECT * FROM foto WHERE (`id` LIKE '%".$code."%'";
-$result = $link->query($query) or die('Error, query failed');
-while($results = mysql_fetch_array($result)){
-     // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-
-         echo "<p><h3>".$results['path'];
-         // posts results gotten from database(title and text) you can also show id ($results['id'])
- }
-
+$query = "SELECT path FROM foto WHERE id=$code";
+$exquery = $link->query($query);
+$row = $exquery-> fetch_assoc();
+$foto_path= $row['path'];
 $link->close();
-
-
-   
-       
