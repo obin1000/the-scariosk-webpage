44829d4568e3b57ab46e7a62983dd5a7dc0b19e8
M. El Hadiyen <mohamed.el.hadiyen@hva.nl>
2017-11-15 15:54:48 +0100

    Update nieuweindex.php - structuur verbeterd en linkje naar opmaak.css gemaakt(De opmaak voor de nieuwe index is in nieuweopmaak.css)

--- nieuweindex.php
+++ nieuweindex.php
@@ -3,30 +3,28 @@
 <html>
 
 <head>
-
-<style>
-	
-
-</style>
-
+        <meta charset="UTF-8">
+        <title>Corendon Scariosk</title>
+        <link rel="icon" href="fotowebsite/icon.png">
+        <link rel="stylesheet" type="text/css" href="opmaak.css">
 </head>
 
 <body>
 
-<div class="inhoud">
-<a href="index.php"><img class="logocorendon" src="logocorendon.png"></a>
-</div>
+    <div class="inhoud">
+        <a href="index.php"><img class="logocorendon" src="logocorendon.png"></a>
+    </div>
 
-<article class="window">
-<form action="/tweedepagina.php" method="post">
-<input class="forminput" type="number" placeholder="Voer code in" name="code">
-<input class="formsubmitbutton" type="submit" value="Haal je foto's" name="invoer">
-</form>
-</article>
+    <article class="window">
+        <form action="/tweedepagina.php" method="post">
+            <input class="forminput" type="number" placeholder="Voer code in" name="code">
+            <input class="formsubmitbutton" type="submit" value="Haal je foto's" name="invoer">
+        </form>
+    </article>
 
-<footer class="footer">
-<p>Copyrights Corendom - 2017</p>
-</footer>
+    <footer class="footer">
+        <p>Copyrights Corendom - 2017</p>
+    </footer>
 
 </body>
 
