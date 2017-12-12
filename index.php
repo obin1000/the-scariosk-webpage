5f54f8af056dcbdada0a4ff89940db088d84d693
M. El Hadiyen <mohamed.el.hadiyen@hva.nl>
2017-12-12 17:56:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,31 @@
 <!DOCTYPE html>
+
 <html>
-    <head>
+
+<head>
         <meta charset="UTF-8">
         <title>Corendon Scariosk</title>
         <link rel="icon" href="fotowebsite/icon.png">
         <link rel="stylesheet" type="text/css" href="opmaak.css">
-    </head>
-    <body>
-        <!--form voor de upload van de foto -->
-        <div class="layout">
-        <form method="post" action="index.php" enctype="multipart/form-data">
-            <img class="logocorendon" src="fotowebsite/logocorendon.jpg" alt="Image can't be displayed">
-            <br />
-            <input class="forminput" type="file" name="foto">
-            <br />
-            <input class="formsubmitbutton" type="submit" name="upload">
+</head>
+
+<body>
+
+    <div class="inhoud">
+        <a href="index.php"><img class="logocorendon" src="logocorendon.png"></a>
+    </div>
+
+    <article class="window">
+        <form action="/tweedepagina.php" method="post">
+            <input class="forminput" type="number" placeholder="Voer uw code in" name="code">
+            <input class="formsubmitbutton" type="submit" value="Haal jouw foto's op!" name="invoer">
         </form>
-        <!--form voor het ophalen van de foto -->
-        <form method="post" action="display.php">
-            <br />
-            <input class="forminput" type="number" name="code" placeholder="Voer uw code in">
-            <br />
-            <input class="formsubmitbutton" type="submit" name="invoer">
-        </form>
-        <?php
-        if (Isset($_POST['upload'])){
-            include 'fileupload.php';
-        }
-        ?>
-        
-        </div>
-    </body>
+    </article>
+
+    <footer class="footer">
+        <p>Copyrights Corendom - 2017</p>
+    </footer>
+
+</body>
+
 </html>
